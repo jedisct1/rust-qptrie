63716c9e0816a5ecadd8691090ed6f1bad296071
Frank Denis <github@pureftpd.org>
2016-11-26 01:37:25 +0100

    Split

---
+++ src/iterator.rs
@@ -0,0 +1,46 @@
+use trie::Node;
+
+pub struct TrieIterator<'s, TK: 's + PartialEq + AsRef<[u8]>, TV: 's> {
+    t: &'s Node<TK, TV>,
+    key: &'s [u8],
+    gt: bool,
+}
+
+impl<'s, TK: 's + PartialEq + AsRef<[u8]>, TV: 's> TrieIterator<'s, TK, TV> {
+    #[inline]
+    pub fn new(t: &'s Node<TK, TV>, key: &'s [u8], gt: bool) -> Self {
+        TrieIterator {
+            t: t,
+            key: key,
+            gt: gt,
+        }
+    }
+}
+
+impl<'s, TK: PartialEq + AsRef<[u8]>, TV> TrieIterator<'s, TK, TV> {
+    #[inline]
+    pub fn different(mut self) -> Self {
+        self.gt = true;
+        self
+    }
+}
+
+impl<'s, TK: PartialEq + AsRef<[u8]>, TV> Iterator for TrieIterator<'s, TK, TV> {
+    type Item = (&'s TK, &'s TV);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = if self.gt {
+            self.t.next_gt(self.key)
+        } else {
+            self.t.next_ge(self.key)
+        };
+        match res {
+            None => None,
+            Some((key, val)) => {
+                self.key = key.as_ref();
+                self.gt = true;
+                Some((key, val))
+            }
+        }
+    }
+}
